4692d0ce592a46c7ffb9ab888bf2ddf6b94494e5
Aline Vignoli de Lima <adelima@questrade.com>
2022-05-16 10:56:57 -0300

    Small fixes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,14 +4,13 @@ import './Card.css';
 const Card = (props) => {
     const [counter, setCounter] = useState(0)
 
-
     return (
         <div 
             className={`Container ${props.active ? "Active" : ""}`} 
             onClick={() => props.onClick(props.id)}
         >
-            <img src="https://picsum.photos/100"/>
-            <h2>{props.title}</h2>
+            <img src="https://picsum.photos/80"/>
+            <h3>{props.title}</h3>
             <div>{props.description}</div>
             <div className='Buttons-group'>
                 <button disabled={counter <= 0} onClick={() => setCounter(counter - 1)}>-</button>

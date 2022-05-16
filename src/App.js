ff56ea4d2aad464d52a1378d913dc020e09fecc0
Aline Vignoli de Lima <adelima@questrade.com>
2022-05-16 13:42:51 -0300

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,41 +1,10 @@
-import { useState } from 'react';
 import './App.css';
-import Card from './components/Card/Card'
-import Header from './components/Header/Header';
-import SideNav from './components/SideNav/SideNav';
-import cards from './data/cards.json'
 
 function App() {
-  const [active, setActive] = useState('')
-
-  const handleActive = (cardId) => setActive(cardId)
-
-  const renderCards = () => {
-    return (
-      cards.map((card) => {
-        return (
-          <Card 
-            id={card.id}
-            key={card.id} 
-            title={card.title} 
-            description={card.description} 
-            active={active === card.id}
-            onClick={handleActive} 
-          />
-        )
-      })
-    )
-  }
   
   return (
     <div className="App">
-      <Header />
-      <div className='Main'>
-        <main className='Card-grid'>
-          {renderCards()}
-        </main>
-        <SideNav cards={cards} onClick={handleActive} active={active} />
-      </div>
+      Hello world!
     </div>
   );
 }
